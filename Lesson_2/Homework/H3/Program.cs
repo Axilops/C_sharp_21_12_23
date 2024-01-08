98560270ac7b2f918718d7962d61de9162697f4f
Axilops <Fafnir12@mail.ru>
2024-01-08 16:13:27 +0300

    решил третью задачу

---
+++ Lesson_2/Homework/H3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число из отрезка [10, 99]:");
+int a = Convert.ToInt32(Console.ReadLine());
+int a1 = a / 10;
+int a2 = a % 10;
+int max = a1 > a2 ? a1 : a2;
+Console.WriteLine(max);
