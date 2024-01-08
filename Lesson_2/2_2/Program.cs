d54f470842c52631bb093483b23abfaa7001ffea
Axilops <Fafnir12@mail.ru>
2024-01-08 15:10:08 +0300

    решил третью задачу

--- Lesson_2/2_2/Program.cs
+++ Lesson_2/2_2/Program.cs
@@ -1,5 +1,7 @@
-﻿int a = int.Parse(Console.ReadLine()!);
+﻿Console.WriteLine("Введите трёхзначное число");
+int a = int.Parse(Console.ReadLine()!);
 int a2 = a % 100 / 10;
 int a3 = a % 10;
 double result = Math.Pow(a2,a3);
+Console.WriteLine("Корень равен");
 Console.WriteLine(result);
