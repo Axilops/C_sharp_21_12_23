e54ea0eaca9f4232b2193f838ac703910c2a2644
Axilops <Fafnir12@mail.ru>
2024-02-03 12:48:59 +0300

    решил вторую задачу

---
+++ Att/2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m,n);
+
+void AkkermanFunction(int n, int m)
+{
+    Console.Write(Akkerman(n, m)); 
+}
+
+int Akkerman(int m, int n)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    else if (m == 0 && n > 0)
+    {
+        return Akkerman(n - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(n - 1, Akkerman(n, m - 1)));
+    }
+}
